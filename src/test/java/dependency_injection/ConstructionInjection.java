af67b1c2abc0b113acfc6984ee8125d987025f18
hackye <838702939@qq.com>
2021-08-25 18:46:12 +0800

    [yaer][DI] feat: add constructionInjection test

---
+++ src/test/java/dependency_injection/ConstructionInjection.java
@@ -0,0 +1,15 @@
+package dependency_injection;
+
+
+import com.thoughtworks.fusheng.integration.junit5.FuShengTest;
+
+@FuShengTest
+public class ConstructionInjection {
+
+    public String retrieveGreeterBless() {
+//        DojoContextUtils.initApplication();
+//        DojoContainer dojoContainer = DojoContextUtils.retrieveDojoContainer();
+//        return dojoContainer.initMessage();
+        return "nothing for now";
+    }
+}
