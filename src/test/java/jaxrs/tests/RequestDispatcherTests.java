cecc828c04e7ac6a49f5410280d931262a2dc408
hackye <838702939@qq.com>
2021-10-30 19:07:58 +0800

    [yaer][JAXRS] test nested class

--- src/test/java/jaxrs/tests/RequestDispatcherTests.java
+++ src/test/java/jaxrs/tests/RequestDispatcherTests.java
@@ -7,6 +7,7 @@ import jaxrs.example.resources.WidgetsResourceWithPathVariable;
 import jaxrs.example.resources.WidgetsResourceWithoutPathVariable;
 import jaxrs.model.RootResourceClassMatchingResult;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import javax.ws.rs.BadRequestException;
@@ -26,35 +27,46 @@ public class RequestDispatcherTests {
         requestDispatcher = new RequestDispatcher();
     }
 
-    @Test
-    void should_identify_candidate_root_resource_classes_matching_the_request_given_single_root_path_without_path_variable() {
-        final String requestUriPath = "/widgets";
-        final List<Class<?>> rootResourceClasses = List.of(WidgetsResourceWithoutPathVariable.class, WidgetResource.class);
+    @Nested
+    class WhenCandidate {
+        @Test
+        void should_identify_candidate_root_resource_classes_matching_the_request_given_single_root_path_without_path_variable() {
+            final String requestUriPath = "/widgets";
+            final List<Class<?>> rootResourceClasses = List.of(WidgetsResourceWithoutPathVariable.class, WidgetResource.class);
 
-        RootResourceClassMatchingResult candidateResourceClasses = requestDispatcher.findCandidateResourceClasses(requestUriPath, rootResourceClasses);
+            RootResourceClassMatchingResult candidateResourceClasses = requestDispatcher.findCandidateResourceClasses(requestUriPath, rootResourceClasses);
 
-        assertThat(candidateResourceClasses.getNotMatchedCapturingGroup()).isNull();
-        assertThat(candidateResourceClasses.getMatchedRootClasses()).containsExactly(WidgetsResourceWithoutPathVariable.class);
+            assertThat(candidateResourceClasses.getNotMatchedCapturingGroup()).isNull();
+            assertThat(candidateResourceClasses.getMatchedRootClasses()).containsExactly(WidgetsResourceWithoutPathVariable.class);
+        }
+
+        @Test
+        void should_identify_candidate_root_resource_classes_matching_the_request_given_root_path_with_path_variable() {
+            final String pathVariable = "1";
+            final String requestUriPath = "/widgets/" + pathVariable;
+            final List<Class<?>> rootResourceClasses = List.of(WidgetsResourceWithPathVariable.class, WidgetResource.class);
+
+            RootResourceClassMatchingResult candidateResourceClasses = requestDispatcher.findCandidateResourceClasses(requestUriPath, rootResourceClasses);
+
+            assertThat(candidateResourceClasses.getNotMatchedCapturingGroup()).isEqualTo(pathVariable);
+            assertThat(candidateResourceClasses.getMatchedRootClasses()).containsExactly(WidgetsResourceWithPathVariable.class);
+        }
+
+        @Test
+        void should_throw_NotFoundException_given_no_matched_root_resource() {
+            final String requestUriPath = "/customer/2066";
+            final List<Class<?>> rootResourceClasses = List.of(WidgetsResourceWithoutPathVariable.class, WidgetResource.class);
+
+            assertThatThrownBy(() -> requestDispatcher.findCandidateResourceClasses(requestUriPath, rootResourceClasses))
+                    .isInstanceOf(RuntimeException.class);
+        }
     }
 
-    @Test
-    void should_identify_candidate_root_resource_classes_matching_the_request_given_root_path_with_path_variable() {
-        final String pathVariable = "1";
-        final String requestUriPath = "/widgets/" + pathVariable;
-        final List<Class<?>> rootResourceClasses = List.of(WidgetsResourceWithPathVariable.class, WidgetResource.class);
+    @Nested
+    class TestNest {
+        @Test
+        void name() {
 
-        RootResourceClassMatchingResult candidateResourceClasses = requestDispatcher.findCandidateResourceClasses(requestUriPath, rootResourceClasses);
-
-        assertThat(candidateResourceClasses.getNotMatchedCapturingGroup()).isEqualTo(pathVariable);
-        assertThat(candidateResourceClasses.getMatchedRootClasses()).containsExactly(WidgetsResourceWithPathVariable.class);
-    }
-
-    @Test
-    void should_throw_NotFoundException_given_no_matched_root_resource() {
-        final String requestUriPath = "/customer/2066";
-        final List<Class<?>> rootResourceClasses = List.of(WidgetsResourceWithoutPathVariable.class, WidgetResource.class);
-
-        assertThatThrownBy(() -> requestDispatcher.findCandidateResourceClasses(requestUriPath, rootResourceClasses))
-                .isInstanceOf(RuntimeException.class);
+        }
     }
 }
